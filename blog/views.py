ea7247512b1cecdb17ee984b26162deb38dee671
athmanziri <issaziri@gmail.com>
2016-11-03 11:00:04 +0300

    django polls index.html works

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import blog
 
 # Create your views here.
 
 from django.http import HttpResponse 
-
-def blog(request):
-    return render(request, 'blog/post_list.html', {})
     
-    
+def post_list(request):
+    posts = blog.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})   
     
 
