7a9a315623219d7c9285c3d3c12bf38d7ebfc8bc
athmanziri <issaziri@gmail.com>
2016-11-03 09:20:46 +0300

    django blog post-list.html created

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse 
+
+def blog(request):
+    return render(request, 'blog/post_list.html', {})
+    
+    
+    
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,6 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
+    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
 ]
